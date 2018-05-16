68cf79d5ae3d1eefdfb7367cc245f951750e0e6c
Previous28 <shujh_sysu@163.com>
2018-05-16 21:56:32 +0800

    [修改] 修改导航栏自适应屏幕高度的实现方式

---
+++ src/components/main/TopLine.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="topLine">
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+.topLine {
+  background:#fbfbfb;
+  width:1044px;
+  height:80px;
+}
+</style>
