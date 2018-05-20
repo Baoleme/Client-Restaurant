ad804f976887b5688042b941a495d3a678d6f079
Previous28 <shujh_sysu@163.com>
2018-05-20 09:13:23 +0800

    [增加] 完成已完成订单列表页以及全部订单列表页

--- src/components/main/Ordered.vue
+++ src/components/main/Ordered.vue
@@ -4,7 +4,19 @@
     <div class="right">
       <TopLine class="top"/>
       <div class="content">
-        <order-menu :filterList="2"/>
+        <order-menu :filterList="2" class="orderMenu" @filter="filter"/>
+        <div class="orderList">
+          <div v-for="(order, index) in filterList" :key="index" class="orderItem">
+            <p><span :class="{cancel: order.curState === '已取消'}">{{order.number}}</span></p>
+            <p><span :class="{cancel: order.curState === '已取消'}">¥{{order.totalPrice}}</span></p>
+            <p><span :class="{cancel: order.curState === '已取消'}">{{order.table}}</span></p>
+            <p><span :class="{cancel: order.curState === '已取消'}">{{order.curState}}</span></p>
+            <p><span :class="{cancel: order.curState === '已取消'}">{{order.createTime}}</span></p>
+            <p><span :class="{cancel: order.curState === '已取消'}">{{order.waitTime}}</span></p>
+            <p><span class="note" :class="{null_: order.note === '无', cancel: order.curState === '已取消'}">{{order.note}}</span></p>
+            <p>查看</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -17,7 +29,53 @@ import OrderMenu from './OrderMenu';
 export default {
   data () {
     return {
+      orderList: [
+        {
+          number: '21937135281',
+          totalPrice: '291.1',
+          table: '23',
+          curState: '已完成',
+          createTime: '2017.2.5 09:30',
+          waitTime: '20:23',
+          note: '不要放葱姜蒜也不要放辣椒，多加陈醋，最好有，不要放葱姜蒜也不要放辣椒，多加陈醋，最好有，不要放葱姜蒜也不要放辣椒，多加陈醋，最好有'
+        },
+        {
+          number: '21937135281',
+          totalPrice: '291.1',
+          table: '23',
+          curState: '已取消',
+          createTime: '2017.2.5 09:30',
+          waitTime: '20:23',
+          note: '无'
+        },
+        {
+          number: '21937135281',
+          totalPrice: '291.1',
+          table: '23',
+          curState: '已完成',
+          createTime: '2017.2.5 09:30',
+          waitTime: '20:23',
+          note: '不要放葱姜蒜也不要放辣椒，多加陈醋，最好有'
+        }
+      ],
+      filterList: []
     };
+  },
+  mounted: function () {
+    this.filterList = this.orderList;
+  },
+  methods: {
+    filter (filterArr) {
+      let list1 = this.orderList.filter(order => (order.curState === '已完成'));
+      let list2 = this.orderList.filter(order => (order.curState === '已取消'));
+      this.filterList = [];
+      if (filterArr[0] === true) {
+        this.filterList = this.filterList.concat(list1);
+      }
+      if (filterArr[1] === true) {
+        this.filterList = this.filterList.concat(list2);
+      }
+    }
   },
   components: {
     MyMenu,
@@ -44,9 +102,81 @@ export default {
 
     .content {
       display: flex;
+      flex-direction: column;
       flex: 10;
       background-color:#f6f6f6;;
       padding: 22px 22px 0 22px;
+
+      .orderMenu {
+        z-index: 10;
+      }
+
+      .orderList {
+        background: #ffffff;
+        flex: 1;
+
+        .orderItem {
+          display: flex;
+          height:45px;
+          padding: 0px 40px 0 31px;
+
+          p {
+            font-family:PingFangSC-Medium;
+            font-size:13px;
+            display: flex;
+            align-items: center;
+            color:#493f3a;
+          }
+
+          p:nth-child(1) {
+            flex:3;
+          }
+          p:nth-child(2) {
+            flex:3;
+          }
+          p:nth-child(3) {
+            flex:2.5;
+          }
+          p:nth-child(4) {
+            flex:3;
+          }
+          p:nth-child(5) {
+            flex:4;
+          }
+          p:nth-child(6) {
+            flex:4;
+          }
+          p:nth-child(7) {
+            flex:5;
+          }
+          p:nth-child(8) {
+            flex:4;
+            justify-content: center;
+            cursor: default;
+          }
+
+          .note {
+            overflow: hidden;
+            text-overflow:ellipsis;
+            white-space: nowrap;
+            width: 14vw;
+            text-align: left;
+          }
+
+          .null_ {
+            color:#9b9b9b;
+            font-family:PingFangSC-Regular;
+          }
+        }
+
+        .cancel {
+          color:#9b9b9b;
+        }
+
+        .orderItem:nth-child(2n+1) {
+          background:#fff8e3;
+        }
+      }
     }
   }
 }
